b4391c3e6de3568733aed02e9a05e9048ddf90f5
Brunn09 <brunnobatista25@gmail.com>
2023-06-03 20:08:48 -0300

    Project finished

--- app/(pages)/profissional/page.js
+++ app/(pages)/profissional/page.js
@@ -1,7 +1,18 @@
-export default function Pro () {
+import Image from 'next/image'
+
+import styles from './pro.module.css'
+
+export default function Pro() {
     return (
-        <div>
-            Profissional
+        <div className={styles.main}>
+            <div className={styles.left}>
+                <h1>Profissional</h1>
+                <p>A Lacrei garante que pessoas LGBTQIAPN + recebam atendimento realizado por profissionais de qualidade e que atendam às suas necessidades de forma segura e acolhedora.</p>
+            </div>
+
+            <div className={styles.right}>
+                <Image src='/profissional.svg' width={100} height={100} alt='Profissional' loading='lazy' />
+            </div>
         </div>
     )
 }
--- app/(pages)/usuario/page.js
+++ app/(pages)/usuario/page.js
@@ -1,7 +1,18 @@
+import Image from 'next/image'
+
+import styles from './user.module.css'
+
 export default function User () {
     return (
-        <div>
-            Usuario
+        <div className={styles.main}>
+            <div className={styles.left}>
+                <h1>Pessoa Usuária</h1>
+                <p>A Lacrei garante que pessoas LGBTQIAPN + recebam atendimento realizado por profissionais de qualidade e que atendam às suas necessidades de forma segura e acolhedora.</p>
+            </div>
+
+            <div className={styles.right}>
+                <Image src='/pessoaUsuariaImg.svg' width={100} height={100} alt='Pessoa Usuária' loading='lazy' />
+            </div>
         </div>
     )
 }
